cb7633706ac0eb49468099ec74dd6b5e9ee0909d
inntonsha <inntonsha@gmail.com>
2021-05-23 15:02:42 +0900

    Finished ourlegacy shop

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
 $('.legacyIndex__main-li:nth-child(1)').hover(function(){
-    $('.legacyIndex__main-img').toggleClass('img__workshop'); 
-})
-
-$('.legacyIndex__main-li:nth-child(2)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__mensNew'); 
 })
 
-$('.legacyIndex__main-li:nth-child(3)').hover(function(){
+$('.legacyIndex__main-li:nth-child(2)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__womensNew'); 
 })
 
-$('.legacyIndex__main-li:nth-child(4)').hover(function(){
+$('.legacyIndex__main-li:nth-child(3)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__118'); 
 })
 
-$('.legacyIndex__main-li:nth-child(5)').hover(function(){
+$('.legacyIndex__main-li:nth-child(4)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__118story'); 
 })
 
-$('.legacyIndex__main-li:nth-child(6)').hover(function(){
+$('.legacyIndex__main-li:nth-child(5)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__big'); 
 })
 
-$('.legacyIndex__main-li:nth-child(7)').hover(function(){
+$('.legacyIndex__main-li:nth-child(6)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__boots'); 
 })
 
-$('.legacyIndex__main-li:nth-child(8)').hover(function(){
+$('.legacyIndex__main-li:nth-child(7)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__wave'); 
 })
 
-$('.legacyIndex__main-li:nth-child(9)').hover(function(){
+$('.legacyIndex__main-li:nth-child(8)').hover(function(){
     $('.legacyIndex__main-img').toggleClass('img__print'); 
+})
+
+$('.legacyIndex__main-li:nth-child(9)').hover(function(){
+    $('.legacyIndex__main-img').toggleClass('img__workshop'); 
 })
 
 $('.legacyIndex__main-li:nth-child(10)').hover(function(){
